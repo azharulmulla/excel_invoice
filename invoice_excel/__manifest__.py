576580bd1129b4e2064659c192aabb89e7f6ce51
azharulmulla <azharul@shorepointsys.com>
2022-04-19 18:50:04 +0600

    invoice will print in excel formate

---
+++ invoice_excel/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Account Import/Export",
+    'summary': """Import/Export Extension for defining the company export/import details""",
+    'author': "SPS",
+    'website': "http://www.shorepointsys.com",
+    'category': 'Account Extra Tools',
+    'version': '0.1',
+    'depends': ['account','documents_sales','account_import_export','report_xlsx'],
+    'data': [
+        'views/report_account_invoice_excel.xml',
+        # 'views/report_template_invoice_excel.xml',
+    ],
+}
---
+++ invoice_excel/models/invoice_xls.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+
+class InvoiceXlsx(models.AbstractModel):
+    _name = 'report.invoice_excel.report_invoice_xls'
+    _inherit = 'report.report_xlsx.abstract'
+
+    def generate_xlsx_report(self, workbook, data, partners):
+        for obj in partners:
+            report_name = obj.name
+            # One sheet by partner
+            sheet = workbook.add_worksheet(report_name[:31])
+            bold = workbook.add_format({'bold': True})
+            sheet.write(0, 0, obj.name, bold)
